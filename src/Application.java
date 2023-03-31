477514fa3233907e86a60d9a670fd5c09494615d
BMoszczynski07 <bmoszczynski2007@gmail.com>
2023-03-31 12:16:08 +0200

    Adding JFrame

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Application extends JFrame {
+    public Application() {
+        setSize(800,600);
+        setLocationRelativeTo(null);
+
+//        initComponents();
+    }
+}
